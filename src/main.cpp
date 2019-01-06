deff35f24f8f7cd6bba91ad70f3b19215a9b389a
non258 <c0117037bf@edu.teu.ac.jp>
2019-01-06 21:24:12 +0900

    雛形の作成

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+
+int n = 10;
+
+void view(std::vector<std::vector<int>> v)
+{
+  for (int i = 1; i < n+1; i++)
+  {
+    for (int j = 1; j < n+1; j++)
+    {
+      if (v[i][j] == 0)
+        std::cout << "□ ";
+      else if (v[i][j] == 1)
+        std::cout << "■ ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+// セルの数を数え上げる
+int count(std::vector<std::vector<int>> v, int x, int y)
+{
+}
+
+// 生死判定を行う
+int live_check(std::vector<std::vector<int>> v, int x, int y)
+{
+}
+
+// mapを書きかえる
+std::vector<std::vector<int>> reload_map(std::vector<std::vector<int>> v)
+{
+}
+
+int main()
+{
+  std::vector<std::vector<int>> cell(12, std::vector<int>(12,0));
+
+  cell[1][2] = 1;
+  cell[2][3] = 1;
+  cell[3][1] = 1;
+  cell[3][2] = 1;
+  cell[3][3] = 1;
+
+  view(cell);
+
+  int w = 0;
+  while(w++ < 100)
+  {
+    view(cell);
+    cell = reload_map(cell);
+    sleep(1);
+  }
+}
